df7c1b79c556fde2029b47c5e603c144361c6d2b
Andrey Glumov <gluman@yandex.ru>
2022-10-12 23:24:17 +0300

    second version - need fix it

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# p – people – команда, которая спросит номер документа и выведет имя человека, которому он принадлежит;
+"""
+ p – people – команда, которая спросит номер документа и выведет имя человека, которому он принадлежит;
 # s – shelf – команда, которая спросит номер документа и выведет номер полки, на которой он находится;
 # Правильно обработайте ситуации, когда пользователь будет вводить несуществующий документ.
 # l– list – команда, которая выведет список всех документов в формате passport "2207 876234" "Василий Гупкин";
@@ -6,34 +7,22 @@
 # d – delete – команда, которая спросит номер документа и удалит полностью документ из каталога и его номер из перечня полок. Предусмотрите сценарий, когда пользователь вводит несуществующий документ;
 # m – move – команда, которая спросит номер документа и целевую полку и переместит его с текущей полки на целевую. Корректно обработайте кейсы, когда пользователь пытается переместить несуществующий документ или переместить документ на несуществующую полку;
 # as – add shelf – команда, которая спросит номер новой полки и добавит ее в перечень. Предусмотрите случай, когда пользователь добавляет полку, которая уже существует.;
-##############################################################
-documents = [
-    {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
-    {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-    {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
-]
 
-directories = {
-    '1': ['2207 876234', '11-2', '5455 028765'],
-    '2': ['10006'],
-    '3': []
-}
-
-
-###############################################################
 # названия переменных в функциях и основном блоке сознательно использую одни и теже. Я понимаю, что в функциоях названия переменных можно было использовать другое.
 # Но так как они в функции переопределены, то имена оставил те же.
+"""
 
-def show_owner(num_doc):
+def show_owner():
+    num_doc = input('Введите номер документа: ')
     for row in documents:
         if num_doc in row.values():
             print(f"Владелец документа: {row['name']}")
             return (1)
-    return (0)
-    # Показать владельца
+        return (0)
 
 
-def show_shelf(num_doc):
+def show_shelf():
+    num_doc = input('Введите номер документа: ')
     for shelf, nums_doc in directories.items():
         if num_doc in nums_doc:
             print(f'Документ находится на полке: {shelf}')
@@ -44,19 +33,24 @@ def show_shelf(num_doc):
 
 def show_docs():
     print('Перечень документов:')
+    row_str = ''
     for row in documents:
-        row_str = ''
         for key in row.keys():
             if key == "type":
                 row_str += row[key]
-            else:
-                row_str += " \"" + row[key] + "\""
+        else:
+            row_str += " \"" + row[key] + "\""
 
-        print(row_str)
+    print(row_str)
     return (1)
 
 
-def add_doc(num_doc, type_doc, name_owner, num_shelf):
+def add_doc():
+    num_doc = input('Введите номер документа: ')
+    type_doc = input('Введите тип документа: ')
+    name_owner = input('Введите имя владельца: ')
+    num_shelf = input('Введите номер полки для хранения: ')
+
     dict_temp = {}  # временный словарь
     if check_doc(num_doc) != 1:  # Инверсия, если документ уже есть, то продложать нельзя.
         if check_shelf(num_shelf) == 1:  # Инверсия, если полки нет, то продложать нельзя.
@@ -79,7 +73,8 @@ def add_doc(num_doc, type_doc, name_owner, num_shelf):
     return (0)
 
 
-def delete_doc(num_doc):
+def delete_doc():
+    num_doc = input('Введите номер документа: ')
     if check_doc(num_doc):
         for row in documents:
             if num_doc in row.values():
@@ -88,11 +83,13 @@ def delete_doc(num_doc):
                         s_docs.remove(num_doc)
                         documents.remove(row)
                         print(f'.Документ с номером {num_doc} удален')
-                        return (1)
-    return (0)
+                        return 1
+        return 0
 
 
-def move_doc(num_doc, num_shelf):
+def move_doc():
+    num_doc = input('Введите номер документа: ')
+    num_shelf = input('Введите номер новой полки для перемещения: ')
     if check_doc(num_doc):
         if check_shelf(num_shelf):
             for s_docs in directories.values():
@@ -100,14 +97,14 @@ def move_doc(num_doc, num_shelf):
                     s_docs.remove(num_doc)
                     directories[num_shelf].append(num_doc)
                     print(f'Документ с номером {num_doc} перемещен на полку №{num_shelf}')
-                    return (1)
+                    return 1
         else:
             if input(f'- Полка с номером {num_shelf} не найдена. Добавить? (yes/no): ').lower() in ['yes', 'y']:
                 add_shelf(num_shelf)
                 return move_doc(num_doc, num_shelf)  # вызов из функции
     else:
         print('- Ошибка перемещения документа')
-        return (0)
+        return 0
 
 
 def show_shelfs():
@@ -116,7 +113,8 @@ def show_shelfs():
     return (1)
 
 
-def add_shelf(num_shelf):
+def add_shelf():
+    num_shelf = input('Введите новый номер полки для создания: ')
     if check_shelf(num_shelf) == 0:
         directories[num_shelf] = []
         print(f'Полка с номером №{num_shelf} добавлена.')
@@ -140,91 +138,51 @@ def check_shelf(num_shelf):  # проверка наличия полки
     return (0)
 
 
-##############################################################
-# Попробовал заложить обработку выполнения команд (функций). Новерное мой подход не совсем верный, думаю, что нужно обработку выполнения функции заложить в самой функции, а выполнеине команды (if add_doc(...) оставить в этом условии. не хватает знаний в best practics.
+def help_fync():
+    print('''
+    "h" - help - вывод справки
+    "p" – people – по номеру документа и выведет имя человека, которому он принадлежит;
+    "s" – shelf – по номеру документа выведет номер полки, на которой он находится;
+    "l" – list – выведет список всех документов в формате passport "2207 876234" "Василий Гупкин";
+    "a" – add – добавление нового документа в каталог и в перечень полок.
+    "d" – delete – по номеру документа удалит полностью документ из каталога и его номер из перечня полок. 
+    "m" – move – по номеру документа и целевой полке переместит его с текущей полки на целевую.
+    "ss" - show shelfs - вывод всех полок с номерами документов
+    "as" – add shelf – по номеру новой полки добавит ее в перечень.
+    "q" - quit - выход из программы
+    ''')
+    return (0)
+
+
+documents = [
+    {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
+    {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
+    {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+]
+
+directories = {
+    '1': ['2207 876234', '11-2', '5455 028765'],
+    '2': ['10006'],
+    '3': []
+}
+
+command = {
+    'h': help_fync(),
+    'p': show_owner(),
+    's': show_shelf(),
+    'l': show_docs(),
+    'd': delete_doc(),
+    'm': move_doc(),
+    'ss': show_shelfs(),
+    'as': add_shelf(),
+    'q': quit()
+}
 
 run = True
 while run:
-    command = input('Введите команду (h или help - справка) -> ')
-
-    if command == 'help' or command == 'h':
-        print('''
-    h - help - вывод справки
-    p – people – по номеру документа и выведет имя человека, которому он принадлежит;
-    s – shelf – по номеру документа выведет номер полки, на которой он находится;
-    l – list – выведет список всех документов в формате passport "2207 876234" "Василий Гупкин";
-    a – add – добавление нового документа в каталог и в перечень полок.
-    d – delete – по номеру документа удалит полностью документ из каталога и его номер из перечня полок. 
-    m – move – по номеру документа и целевой полке переместит его с текущей полки на целевую.
-    ss - show shelfs - вывод всех полок с номерами документов
-    as – add shelf – по номеру новой полки добавит ее в перечень.
-    q - quit - выход из программы
-    ''')
-
-    elif command == 'people' or command == 'p':
-        num_doc = input('Введите номер документа: ')
-        if show_owner(num_doc):
-            print('...Выполнено.')
-        else:
-            print("Документ не найден!")
-
-    elif command == 'shelf' or command == 's':
-        num_doc = input('Введите номер документа: ')
-        if show_shelf(num_doc):
-            print('..Выполнено.')
-        else:
-            print('Документ не найден, полка не найдена!')
-
-    elif command == 'list' or command == 'l':
-        if show_docs():
-            print('..Выполнено.')
-        else:
-            print('-Ошибка выполнения запроса!')
-
-    elif command == 'add' or command == 'a':
-        num_doc = input('Введите номер документа: ')
-        type_doc = input('Введите тип документа: ')
-        name_owner = input('Введите имя владельца: ')
-        num_shelf = input('Введите номер полки для хранения: ')
-        if add_doc(num_doc, type_doc, name_owner, num_shelf):
-            print('...Документ добавлен')
-        else:
-            print('Ошибка добавления документа!')
-
-    elif command == 'delete' or command == 'd':
-        num_doc = input('Введите номер документа: ')
-        if delete_doc(num_doc):
-            print('..Выполнено')
-        else:
-            print('- Ошибка!')
-
-    elif command == 'move' or command == 'm':
-        num_doc = input('Введите номер документа: ')
-        num_shelf = input('Введите номер новой полки для перемещения: ')
-        if move_doc(num_doc, num_shelf):
-            print('..Выполнено.')
-        else:
-            print('- Ошибка!')
-
-    elif command == 'show shelfs' or command == 'ss':
-        if show_shelfs():
-            print('..Выполнено.')
-        else:
-            print('- Ошибка!')
-
-    elif command == 'add shelf' or command == 'as':
-        num_shelf = input('Введите новый номер полки для создания: ')
-        if add_shelf(num_shelf):
-            print('..Выполнено.')
-        else:
-            print('- Ошибка.')
-
-    elif command == 'quit' or command == 'q':
-        print('До свидания!')
-        run = False
-
-    else:
-        print('Командра не распознана. help - вывод команд, quit - выход.')
+    # print('Введите команду (h или help - справка)')
+    var = input(f'Введите команду (h или help - справка) {command[input()]}')
+    print(var)
 
 
 
